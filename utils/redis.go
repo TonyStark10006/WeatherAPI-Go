c52d06c9de49b1d1eeafe93bba19b7f0414e7897
TonyStark10006 <TonyStark10006@gmail.com>
2018-10-14 23:13:33 +0800

    update something

--- utils/redis.go
+++ utils/redis.go
@@ -6,8 +6,10 @@ import (
 	"github.com/go-redis/redis"
 )
 
+var client *redis.Client
+
 func init() {
-	client := redis.NewClient(&redis.Options{
+	client = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
@@ -15,4 +17,26 @@ func init() {
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
+}
+
+func getAValue(key string) (value string, err error) {
+	val, err := client.Get(key).Result()
+	if err == redis.Nil {
+		fmt.Println(key + " does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+
+	return val, err
+}
+
+func setAValue(key string, value interface{}) (err error) {
+	err = client.Set(key, value, 0).Err()
+	if err != nil {
+		panic(err)
+	} else {
+		err = nil
+	}
+
+	return err
 }
